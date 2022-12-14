aa3f5757cf29d2051540aca97f6d92e3ab0a7637
Awsal98 <awsalaladwani98@gmail.com>
2022-12-14 19:59:01 +0300

    done

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ let all_grades_div = document.getElementById("all-grades");
 let random_grade_div = document.getElementById("random-grade");
 /* لا تقم بتعديل الأسطر المكتوبة بالأعلى */
 
-
 /*  الجزء الأول والثاني من التمرين */
+grades.pop()
+grades.push(99)
+console.log(grades)
+grades.forEach( item => {
+  all_grades_div.innerHTML += `<span>${item}<span>` });
+ 
+let randomIndex = Math.floor(Math.random() * grades.length)
 
-
+random_grade_div.innerHTML = `<span>#${randomIndex}: ${grades[randomIndex]}<span>`
 
 
 
@@ -27,11 +33,23 @@ let course = {
 
 //Change studentsCount,track property here
 
-
+ track = "web",
+ studentsCount = 40,
+ 
 
 //Console log here
-
+console.log("Hello my name is awsal i'm in "+ track+ " at " + course.name)
 
 
 //Create student object here
+let student= {
+  name: "awsal",
+  university: "kuwait university"
+}
 
+let UniCODE = {
+name: "awsal" ,
+track : "web",
+location :" kuwait unversity",
+
+}
